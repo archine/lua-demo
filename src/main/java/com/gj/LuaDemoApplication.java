0c34eb7d04fab309689ec34f7d8441437300824a
guojing <a87096937@qq.com>
2019-06-19 16:56:30 +0800

    '常用数据类型基本用法'

---
+++ src/main/java/com/gj/LuaDemoApplication.java
@@ -0,0 +1,13 @@
+package com.gj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuaDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LuaDemoApplication.class, args);
+    }
+
+}
